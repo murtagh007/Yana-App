89fa2f848a365140f82b7b48a2e16a07954f506c
Aditya Satpathy <adi.snowkid@gmail.com>
2025-08-24 23:47:34 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,19 +1,22 @@
-// Firebase v8 style config and initialization
+// firebase-config.js
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyCmmmJh7xhwJ5S_aa5gDpf1z37lg2JGGu8",
   authDomain: "yana-app-43d76.firebaseapp.com",
   projectId: "yana-app-43d76",
-  storageBucket: "yana-app-43d76.appspot.com", // corrected domain for storage
+  storageBucket: "yana-app-43d76.firebasestorage.app",
   messagingSenderId: "746812065904",
   appId: "1:746812065904:web:196a90926c59edf9468cb5",
   measurementId: "G-W7E6WB8Y6D"
 };
 
 // Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
 
-// Initialize services
-const db = firebase.firestore();
-const auth = firebase.auth();
